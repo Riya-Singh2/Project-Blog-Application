afd319f9b6c4508f1adf6cc8efa4ae8399117fe6
unknown <2riya.6j@gmail.com>
2025-03-20 13:56:00 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+//component
+import Login from './component/account/login';
+
+
+
+function App() {
+  return (
+    <div style={{marginTop : 64}}>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
